55afdc6c3df293bbf6a1fcf999463e62d24c5d73
chihiro-kakizaki <kkzkchr@gmail.com>
2022-07-06 13:40:10 +0900

    add:ajaxでリクエストそうションまでの処理追記

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,18 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+$(function () {
+  $('.js-text_field').on('keyup', function () {
+    let title = $.trim($(this).val());
+
+    $.ajax({
+      type: 'GET',
+      url: '/messages/searches',
+      data: { title: title },
+      dataType: 'json'
+    })
+  });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
